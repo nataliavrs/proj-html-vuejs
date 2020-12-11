d54604cce9cac1e51683abd94bad7d9752a753e4
nataliavrs <verasnatali4@gmail.com>
2020-12-11 17:00:35 +0100

    Consegna esercizio

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
-    titles: ['About', 'Twitter', 'Important Links', 'Contact me'],
+    // NAVIGATION BAR
+    navLinks: ['homes', 'pages','blog','shop','events','elements'],
+    // FOOTER
+    // footer sections titles
+    titles: ['about', 'twitter', 'important links', 'contact me'],
+    // tweets section
     tweets: [
-    {
-      username: '@aliasadm',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing. '
-    },
-    {
-      username: '@HenrySefaBoaky',
-      message: 'Hi, can you please submit a ticket at https'
-    }
-  ],
-    links: ['About', 'About us', 'Language packs', 'Become a coach', 'Monthly events'],
+      {
+        username: '@aliasadm',
+        message: 'Lorem ipsum dolor sit amet, consectetur adipisicing. '
+      },
+      {
+        username: '@HenrySefaBoaky',
+        message: 'Hi, can you please submit a ticket at https'
+      }
+    ],
+    // links section
+    footerLinks: ['about', 'about us', 'language packs', 'become a coach', 'monthly events'],
+    // contact me section 
     contact: [
       {
         address: '457 BigBlue Street, NY 10013',
@@ -21,10 +28,4 @@ var app = new Vue({
       },
     ]
   },
-  mounted: function () {
-
-  },
-  methods: {
-
-  }
 });

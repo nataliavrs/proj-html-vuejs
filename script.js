ecd831a6fb3dd6a1a224f4d4cf13dcb7b12bf127
nataliavrs <verasnatali4@gmail.com>
2020-12-11 16:35:27 +0100

    Contact us content, footer dynamic

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 var app = new Vue({
   el: '#app',
   data: {
-
+    titles: ['About', 'Twitter', 'Important Links', 'Contact me'],
+    tweets: [
+    {
+      username: '@aliasadm',
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing. '
+    },
+    {
+      username: '@HenrySefaBoaky',
+      message: 'Hi, can you please submit a ticket at https'
+    }
+  ],
+    links: ['About', 'About us', 'Language packs', 'Become a coach', 'Monthly events'],
+    contact: [
+      {
+        address: '457 BigBlue Street, NY 10013',
+        number: '(315) 5512-2579',
+        email: 'everlead@mikado.com',
+      },
+    ]
   },
   mounted: function () {
 
@@ -10,6 +28,3 @@ var app = new Vue({
 
   }
 });
-
-
-console.log('debug');
